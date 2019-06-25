98f8607e1930acdbb10a3b12fcc02448b05940e1
Kevin Bennett <kev@drule.org>
2019-06-25 00:46:11 -0600

    fix redis pw

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 if Padrino.env != :test
-  $REDIS_ = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+  $REDIS_ = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'],password: "redis" )
 
   $REDIS = Redis::Namespace.new(:fw, redis: $REDIS_)
 
   $QUEUE1 = Redis::Queue.new('queue1', 'bp__queue1', redis: $REDIS)
 
-  $REDIS_PUBSUB_R = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+$REDIS_PUBSUB_R = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], password: "redis")
 
   $REDIS_PUBSUB_READ = Redis::Namespace.new(:fw, redis: $REDIS_PUBSUB_R)
 end
